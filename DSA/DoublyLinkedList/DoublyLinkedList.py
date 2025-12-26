398a0d1cf6de218ef4bda41028154c28a9050768
SaiShravanthReddy <82753425+SaiShravanthReddy@users.noreply.github.com>
2025-12-25 22:05:22 -0800

    def add_after_node(self, key, data):

--- DSA/DoublyLinkedList/DoublyLinkedList.py
+++ DSA/DoublyLinkedList/DoublyLinkedList.py
@@ -47,7 +47,48 @@ class DoublyLinkedList:
         new_node.next = self.head
         self.head = new_node
         
+    def add_after_node(self, key, data):
+        # Took 5 mins
+            # check if dll is none or not
+            # traverse to the key 
+            # point new_node to next node 
+            # and current node as prev
+            # then the other two
+
+        # Took 5 mins - edge cases
+            # if append on end node
+
+        if not self.head:
+            return 
+
+        cur = self.head
+
+        while cur and cur.data != key:
+            cur = cur.next
+
+        if cur == None:
+            print("Key not found in the Linked List")
+            return 
+
+        new_node = Node(data)
+
+        new_node.next = cur.next
+        new_node.prev = cur
+
+        # cur.next = new_node
+        # because of the edge case handle, - Change in logic
+        # this line should have been below the condition
+
+        # for adding at the end of linked list
+        if cur.next:
+            cur.next.prev = new_node
+            
+        cur.next = new_node
+
+
+    def add_before_node(self, key, data):
     
+
 
 dllist = DoublyLinkedList()
 dllist.prepend(0)
