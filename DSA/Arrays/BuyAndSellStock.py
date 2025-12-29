11fd84dcf86ec8c42039e32f506b4caaa0960f9b
SaiShravanthReddy <82753425+SaiShravanthReddy@users.noreply.github.com>
2025-12-29 13:48:58 -0800

    Add solution for Best Time to Buy and Sell Stock
    Implements the maxProfit method in BuyAndSellStock.py to solve LeetCode problem 121. The algorithm tracks the lowest and highest prices to calculate the maximum profit from a single buy-sell transaction.

---
+++ DSA/Arrays/BuyAndSellStock.py
@@ -0,0 +1,35 @@
+# 121. Best Time to Buy and Sell Stock
+# 20 mins to read, solve and debug
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # return if len of array is 0,1
+        # vars - low, high, profit = 0,0,0
+        # initial val l, h = first element
+        # traverse the list
+        # if val is less than l, replace l and h with val and cal profit
+        # if val is higher than h, replace h and cal proit
+        # if neither contnue
+        # finally return profit
+
+        low, high = prices[0], prices[0]
+        profit = 0
+
+        for index in range(1, len(prices)):
+
+            if prices[index] < low:
+                low = prices[index]
+                high = prices[index]
+
+                if (high - low) > profit:
+                    profit = high - low
+
+            elif prices[index] > high:
+                high = prices[index]
+                
+                if (high - low) > profit:
+                    profit = high - low
+            else:
+                continue
+        
+        return profit
