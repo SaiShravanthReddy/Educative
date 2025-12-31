45323dd936a396ae79d8732dbb3e50e06683370b
SaiShravanthReddy <82753425+SaiShravanthReddy@users.noreply.github.com>
2025-12-30 19:24:59 -0800

    Add solution for Subarrays Distinct Element Sum of Squares
    Implements a brute-force O(n^2) approach for Leetcode problem 2913, calculating the sum of squares of distinct elements in all subarrays.

---
+++ Leetcode/HashTable/SubarraysDistinctElementSumOfSquares1.py
@@ -0,0 +1,27 @@
+# 2913. Subarrays Distinct Element Sum of Squares 1
+# 14 mins to read and solve
+# O(n^2) solution
+
+class Solution:
+    def sumCounts(self, nums: List[int]) -> int:
+        # count += len for subarry size 1
+        # construct 2 len sbarrays and so on till full len
+        # convert each size array to set and count elements in set and square them and add them
+
+        ans = len(nums)
+
+        size = 1
+
+        while size < len(nums):
+            first = 0
+            second = first + size
+
+            while second < len(nums):
+                ans += (len(set(nums[first:second+1])))**2
+
+                first += 1
+                second += 1
+
+            size += 1
+
+        return ans
